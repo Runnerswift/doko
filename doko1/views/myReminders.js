f85bc1262d1dda4c326b1725dfa357c7481d3a6b
The Runnerswift <tobitaylor1982@gmail.com>
2018-08-20 18:01:13 -0600

    updated myReminders files to include modal

--- doko1/views/myReminders.js
+++ doko1/views/myReminders.js
@@ -6,10 +6,25 @@ onload = (function() {
     };
 
   var newIcon = document.getElementById("newIcon");
-    newIcon.onclick = function() {
-      window.location.replace("/createReminder")
-    };
-
+    var modal = document.querySelector(".modal");
+    var trigger = document.querySelector(".trigger");
+    var closeButton = document.querySelector(".close-button");
+  
+      function toggleModal() {
+        modal.classList.toggle("show-modal");
+      }
+  
+      function windowOnClick(event) {
+        if (event.target === modal) {
+          toggleModal();
+        }
+      }
+  
+      newIcon.addEventListener("click", toggleModal);
+      closeButton.addEventListener("click", toggleModal);
+      window.addEventListener("click", windowOnClick);
+      });
+    
   var reminders = document.getElementById("remindersIcon")
     reminders.onclick = function() {
       window.location.replace("/myReminders");
@@ -19,4 +34,3 @@ onload = (function() {
     logout.onclick = function() {
       window.location.replace("/");
     };
-});
